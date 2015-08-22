1c98b90945996ebe3c0280a2a067bb8a983e63c8
sebastianrs95 <sebastianrs95@gmail.com>
2015-08-22 10:14:28 -0700

    cambiando n por x en la funcion factorial

--- sebastian.py
+++ sebastian.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # Aqui va código de Sebastián
 
-def factorial(n):
-	if n < 2:
+def factorial(x):
+	if x < 2:
 		return 1
 	else:
-		return n * factorial(n-1)
+		return x * factorial(x-1)
 
