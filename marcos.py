3b1663315674ce295f9214e8f84f7feb999031f7
uzumarkus <uzumarkus@gmail.com>
2015-08-22 11:47:36 -0700

    multiplicacion de matrices aleatorias

---
+++ marcos.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+matriz = np.random.rand(3,3)
+matriz2 = np.random.rand(3,3)
+
+print "mulplicacion punto a punto de dos matrices aleatorias"
+print 	"A: \n", matriz
+print "B: \n", matriz2
+print "AxB: \n", np.dot(matriz,matriz2)
